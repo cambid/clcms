972f570e3e0626d0578a76304d6655df0cb3cd2c
Jelte Jansen <Tjebbe@kanariepiet.com>
2006-03-09 19:47:47 +0000

    new wiki markup in examples

--- bin/convert_wiki_style.py
+++ bin/convert_wiki_style.py
@@ -26,33 +26,11 @@ def wiki_to_new_simple(line):
 	    line += "\n"
 	    return line
     elif line[:1] == '*' and line[-1:] == '*' and line != "*":
-        print "Line: "+line
-        print "To:   " + "'''" + line[1:-1] + "'''\n"
         return "'''" + line[1:-1] + "'''\n"
     elif line[:1] == '.' and line[-1:] == '.' and line != ".":
         return "''" + line[1:-1] + "''\n"
     elif line[:1] == '=' and line[-1:] == '=' and line != "=":
         return "__" + line[1:-1] + "\n"
-    # replace links and image refs
-#    adv_link_p = re.compile('\[\[(.*?)\]\[(.*?)\](?:\[(.*?)\])?\]')
-#    adv_m = adv_link_p.search(line)
-#    if adv_m:
-#        name = line[adv_m.start(2):adv_m.end(2)]
-#        href = escape_url(line[adv_m.start(1):adv_m.end(1)])
-#        extra = ""
-#        if len(adv_m.groups()) > 2:
-#        	extra = " " + line[adv_m.start(3):adv_m.end(3)]
-#        if href[0] == ':':
-#        	targetPage = None
-#        	if page:
-#        		targetPage = page.getRootPage().findPageByID(href[1:])
-#		if not targetPage:
-#			print "Page with ID '"+href[1:]+"' not found. Quitting."
-#			sys.exit(1)
-#		if name == "":
-#			name = targetPage.name
-#		href = page.getBackDir() + targetPage.getTotalOutputDir() + "index.html"
-#        line = line[:adv_m.start()] + "<a href=\"" + href + extra + "\">" + name + "</a>" + line[adv_m.end():]
 
     img_p = re.compile('\{\{(.*?)\}\{(.*?)\}(?:\{(.*?)\})?\}')
     img_m = img_p.search(line)
@@ -79,116 +57,6 @@ def wiki_to_new(wiki_lines):
 		else:
 			new_lines.append(l)
 	return new_lines
-
-def old_wiki_to_html(wiki_lines, page = None):
-    html_lines = []
-    i = 0
-    no_wiki = False
-    whitespace_p = re.compile('^\s*\n$')
-    while i < len(wiki_lines):
-        line = wiki_lines[i]
-        if no_wiki:
-            if line[:14] == "_NO_WIKI_END_\n":
-                no_wiki = False
-            else:
-                html_lines.append(line)
-        else:
-            if line[:10] == "_NO_WIKI_\n":
-                no_wiki = True
-            elif line[:3] == "---":
-                j = 3
-                while line[j] == "+":
-                    j += 1
-                if (j > 3):
-		    html_lines.append((j-3)*"=" + wiki_to_html_simple(line[j:]) + (j-3)*"=")
-                    #html_lines.append("<h" + str(j-3) + ">" + wiki_to_html_simple(line[j:], page) + "</h" + str(j-3) +  ">\n")
-                else:
-                    html_lines.append(wiki_to_html_simple(line, page))
-            elif line[:5] == "   * ":
-                html_lines.append("<ul>\n")
-                html_lines.append("\t<li>\n")
-                html_lines.append(wiki_to_html_simple(line[5:], page))
-                in_list1 = True
-                while in_list1:
-                    i += 1
-                    if i < len(wiki_lines):
-                        line = wiki_lines[i]
-                        if line[:5] == "   * ":
-                            html_lines.append("\t</li>\n")
-                            html_lines.append("\t<li>\n")
-			    html_lines.append(wiki_to_html_simple(line[5:], page))
-                        elif line[:8] == "      * ":
-                            html_lines.append("<ul>\n")
-                            html_lines.append("\t<li>\n")
-                            html_lines.append(wiki_to_html_simple(line[8:], page))
-                            in_list2 = True
-                            while in_list2:
-                                i += 1
-                                if i < len(wiki_lines):
-                                    line = wiki_lines[i]
-                                    if line[:8] == "      * ":
-                                        html_lines.append("\t</li>\n")
-                                        html_lines.append("\t<li>\n")
-                                        html_lines.append(wiki_to_html_simple(line[8:], page))
-                                    #elif line[:8] == "      * ":
-                                    elif line[:11] == "         * ":
-                                        html_lines.append("<ul>\n")
-                                        html_lines.append("\t<li>\n")
-                                        html_lines.append(wiki_to_html_simple(line[11:], page))
-                                        in_list3 = True
-                                        while in_list3:
-                                            i += 1
-                                            if i < len(wiki_lines):
-                                                line = wiki_lines[i]
-                                                if line[:11] == "         * ":
-                                                    html_lines.append("\t</li>\n")
-                                                    html_lines.append("\t<li>\n")
-                                                    html_lines.append(wiki_to_html_simple(line[11:], page))
-                                                #elif line[:11] == "         * ":
- 					        elif whitespace_p.match(line):
-							html_lines.append("<br />\n")
-                                                elif line[:11] == "           ":
-							html_lines.append(wiki_to_html_simple(line[11:], page))
-                                                else:
-                                                    html_lines.append("\t</li>\n")
-                                                    html_lines.append("</ul>\n")
-                                                    in_list3 = False
-                                                    i -= 1
-                                            else:
-                                                html_lines.append("\t</li>\n")
-                                                html_lines.append("</ul>\n")
-                                                in_list3 = False
-   				    elif whitespace_p.match(line):
-					html_lines.append("<br />\n")
-                                    elif line[:8] == "        ":
-					html_lines.append(wiki_to_html_simple(line[8:], page))
-                                    else:
-                                        html_lines.append("\t</li>\n")
-                                        html_lines.append("</ul>\n")
-                                        in_list2 = False
-                                        i -= 1
-                                else:
-                                    html_lines.append("\t</li>\n")
-                                    html_lines.append("</ul>\n")
-                                    in_list2 = False
-			elif whitespace_p.match(line):
-                            	html_lines.append("<br />\n")
-                        elif line[:5] == "     ":
-                                html_lines.append(wiki_to_html_simple(line[5:], page))
-                        else:
-                            html_lines.append("\t</li>\n")
-                            html_lines.append("</ul>\n")
-                            in_list1 = False
-                            i -= 1
-                    else:
-                        html_lines.append("\t</li>\n")
-                        html_lines.append("</ul>\n")
-                        in_list1 = False
-            else:
-                html_lines.append(wiki_to_html_simple(line, page))
-        i += 1
-    return html_lines
-
 
 def usage():
 	print "Usage: convert_wiki_style.py [OPTIONS]"
@@ -291,6 +159,5 @@ else:
 				o_file.close()
 				shutil.copystat(bakfilename, pagefilename)
 
-#print all_file_list
 
 
